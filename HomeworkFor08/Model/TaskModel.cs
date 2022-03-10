8ceb774904d0531af9ec79443fc63ddfd3ddd5f1
witey <witey@WITEY>
2022-03-10 09:40:26 +0400

    Add project files.

---
+++ HomeworkFor08/Model/TaskModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkFor08.Model
+{
+    public class TaskModel
+    {
+        private string Description;
+
+        public string MyProperty
+        {
+            get { return Description; }
+            set { Description = value; }
+        }
+
+        private bool isFinished;
+
+        public bool IsFinished
+        {
+            get { return isFinished; }
+            set { isFinished = value; }
+        }
+
+
+
+        public TaskModel(string _Description)
+        {
+            Description = _Description;
+            isFinished = false;
+        }
+    }
+}
---
+++ HomeworkFor08/ViewModel/TaskViewModel.cs
@@ -0,0 +1,42 @@
+﻿using HomeworkFor08.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HomeworkFor08.ViewModel
+{
+    public class TaskViewModel /*: INotifyPropertyChanged*/
+    {
+        List<TaskModel> tasks = new();
+
+
+        public void addTask(string _description)
+        {
+            if (_description == null) {
+                MessageBox.Show("No description, cannot create a Task."); 
+            }
+
+            else
+            {
+                TaskModel newTask = new TaskModel(_description);
+                tasks.Add(newTask);
+            }
+        }
+
+        public void deleteTask()
+        {
+           //delete chosen task
+        }
+
+        public void finishTask()
+        {
+           //check chosen task
+        }
+
+
+    }
+}
